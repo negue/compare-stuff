9b81b9c603d5e42b414d3ed0700939c6e3638eb8
negue <eugen.bolz@gmail.com>
2023-08-12 18:46:54 +0200

    hover column and rows to see where you at

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -5,17 +5,19 @@
 
     export let tableContextState: TableContextState;
 
+    let currentHighlightedColumn: string| null = null;
+
     // https://stackoverflow.com/a/66490611
 
     $: columns = R.sortBy($tableContextState.columns, c => c.order);
     $: rows = R.sortBy($tableContextState.rows, r => r.order);
 </script>
-
-<table>
+<table on:pointerleave={() => currentHighlightedColumn = null}>
     <tr>
         <td></td>
         {#each columns as column (column.id)}
-        <th>
+        <th on:pointerenter={() => {currentHighlightedColumn = column.id}}
+            class:isHovered={column.id === currentHighlightedColumn}>
             <LabelEditable data={column.label}
                            on:update={(newData) => {tableContextState.editColumn(column.id, newData.detail)}} />
         </th>
@@ -33,7 +35,8 @@
                            on:update={(newData) => tableContextState.editRow(row.id, newData.detail)} />
         </td>
         {#each columns as column (`${row.id}_${column.id}`)}
-            <td>
+            <td on:pointerenter={() => {currentHighlightedColumn = column.id}}
+                class:isHovered={column.id === currentHighlightedColumn}>
                 <LabelEditable bind:data={$tableContextState.cells[`${row.id}_${column.id}`]}></LabelEditable>
             </td>
         {/each}
@@ -49,3 +52,14 @@
     </tr>
 </table>
 
+<style lang="scss">
+    tr {
+      &:hover {
+        background: #ffffff47;
+      }
+    }
+
+    .isHovered {
+      background: #ffffff47;
+    }
+</style>
